e2f5c1d0a391e78a78ee1df6fcce0fd9566c9074
Erickira <ericgodard1@hotmail.fr>
2017-05-23 16:38:07 +0200

    Commit ajoutant et réparant la connection

--- src/com/tpJava/model/LoginDao.java
+++ src/com/tpJava/model/LoginDao.java
@@ -4,14 +4,21 @@ package com.tpJava.model;
  * Created by Erickira on 23/05/2017.
  */
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import com.mysql.jdbc.*;
 public class LoginDao {
     public static boolean validate(String name,String pass){
         boolean status=false;
         try{
+            // Step 1: Allocate a database Connection object
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection(
-                    "mysql://localhost/projetJEE:1521:xe","root","");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/projetjee", "root", ""); // <== Check!
+            // database-URL(hostname, port, default database), username, password
+
+            // Step 2: Allocate a Statement object within the Connection
 
             PreparedStatement ps=con.prepareStatement(
                     "select * from userreg where name=? and pass=?");
@@ -21,7 +28,12 @@ public class LoginDao {
             ResultSet rs=ps.executeQuery();
             status=rs.next();
 
-        }catch(Exception e){System.out.println(e);}
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return status;
     }
 }
