a59b329bab5a3ed089852f04e84b9b7b7c808487
Erickira <ericgodard1@hotmail.fr>
2017-05-24 09:01:15 +0200

    Ajout singleton (Non utilisé)

--- src/com/tpJava/model/LoginDao.java
+++ src/com/tpJava/model/LoginDao.java
@@ -6,9 +6,13 @@ package com.tpJava.model;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 import com.mysql.jdbc.*;
 public class LoginDao {
+    public Connection conn;
+    private Statement statement;
+    public static LoginDao db;
     public static boolean validate(String name,String pass){
         boolean status=false;
         try{
@@ -36,4 +40,58 @@ public class LoginDao {
         }
         return status;
     }
+    /**
+     * @desc A singleton database access class for MySQL
+     * @author Ramindu
+     */
+        /**
+         *
+         * @return MysqlConnect Database connection object
+         */
+        public static synchronized LoginDao getDbCon() {
+            if ( db == null ) {
+                db = new LoginDao();
+            }
+            return db;
+
+        }
+        /**
+         *
+         * @param query String The query to be executed
+         * @return a ResultSet object containing the results or null if not available
+         * @throws SQLException
+         */
+        public ResultSet query(String query) throws SQLException{
+            statement = db.conn.createStatement();
+            ResultSet res = statement.executeQuery(query);
+            return res;
+        }
+        /**
+         * @desc Method to insert data to a table
+         * @param insertQuery String The Insert query
+         * @return boolean
+         * @throws SQLException
+         */
+        public int insert(String insertQuery) throws SQLException {
+            statement = db.conn.createStatement();
+            int result = statement.executeUpdate(insertQuery);
+            return result;
+
+        }
+
+    private LoginDao() {
+        String url= "jdbc:mysql://localhost:3306/";
+        String dbName = "projetjee";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "root";
+        String password = "";
+        try {
+            Class.forName(driver).newInstance();
+            this.conn = (Connection)DriverManager.getConnection(url+dbName,userName,password);
+        }
+        catch (Exception sqle) {
+            sqle.printStackTrace();
+        }
+    }
 }
+
